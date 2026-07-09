2a0213d4824bd9c55f3889ea9ea373486e1c6804
Dheeraj-813 <dheeraj.parab813@gmail.com>
2026-07-09 17:27:59 +0530

    NotFound Page Complete

--- Front-End/vite-project/src/Pages/NotFound.jsx
+++ Front-End/vite-project/src/Pages/NotFound.jsx
@@ -1,7 +1,11 @@
+import pageNotFound from '../assets/PageNotFound.png'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className="min-h-screen bg-cover bg-center flex items-center justify-center"
+      style={{ backgroundImage: `url(${pageNotFound})` }}>
+      {/* <h1 className="text-gray-500">Page Not Found</h1> */}
+    </div>
   )
 }
 
