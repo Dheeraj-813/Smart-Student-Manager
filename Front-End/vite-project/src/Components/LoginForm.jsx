08e95c655084d044f2f7c965eeb771d1c60fa5f5
Dheeraj-813 <dheeraj.parab813@gmail.com>
2026-07-09 13:17:31 +0530

    Login Layout Created

--- Front-End/vite-project/src/Components/LoginForm.jsx
+++ Front-End/vite-project/src/Components/LoginForm.jsx
@@ -1,6 +1,7 @@
 
 import studentsCap from '../assets/students-cap.png';
 import studManage from '../assets/StudentManage.jpg'
+import { Link } from 'react-router-dom';
 
 function LoginForm() {
   return (
@@ -24,7 +25,7 @@ function LoginForm() {
         {/* Right side - Login form */}
         <div className="md:w-1/2 flex items-center justify-center">
             <form className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-                <h2 className="text-2xl font-bold mb-4">Welcome Back
+                <h2 className="text-2xl font-bold mb-4">Welcome Back<br />
 
                     Sign in to continue to Smart Student Manager</h2>
                 <div className="mb-4">
@@ -50,27 +51,24 @@ function LoginForm() {
                     />
                 </div>
 
-                <div className='mb-4'>
-                    <label className="inline-flex items-center">
-                        <a href="/forgot-password" className="text-sm text-blue-500 hover:text-blue-700 underline">
+                <div className='flex justify-between items-center mt-4'>
+                        <Link to="/forgot-password" className="text-sm text-blue-500 hover:text-blue-700">
                             Forgot Password?
-                        </a>
-                    </label>
-                </div>
+                        </Link>
 
-                <div className='mb-4'>
-                    <label className="inline-flex items-center">
+                    <label className="flex items-center gap-2">
                         <input
                             type="checkbox"
                             id="remember"
                             className="form-checkbox h-4 w-4 text-blue-500 focus:ring-blue-500 border-gray-300 rounded"
                         />
-                        <span className="ml-2 text-sm text-gray-600">Remember me</span>
+                        <span className="text-sm text-gray-600">Remember me</span>
                     </label>
                 </div>
-                <div className="flex items-center justify-between">
+
+                <div className="items-center justify-between mt-6">
                     <button
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                         type="submit"
                     >
                         Sign In
