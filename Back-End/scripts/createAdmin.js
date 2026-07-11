e7efdd8d09598d5f275d0e62a0e9d14faad1cacd
Dheeraj-813 <dheeraj.parab813@gmail.com>
2026-07-11 14:15:53 +0530

    scripts added

---
+++ Back-End/scripts/createAdmin.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const connectDB = require('../config/db');
+const User = require('../models/User');
+
+const createAdmin = async () => {
+    try{
+
+        // connect database
+        await connectDB();
+
+        // check whether admin already exists
+        const existingUser = await User.findOne({
+            email: "admin@gmail.com"
+        });
+
+        if(existingUser){
+            console.log("Admin already exists");
+            process.exit();
+        }
+
+        // Encrypt password
+        const hashedPassword = await bcrypt.hash('admin123', 10);
+
+        // Create Admin
+        const admin = await User.create({
+            name: 'Admin',
+            email: "admin@gmail.com",
+            password: hashedPassword,
+            role: 'admin'
+        });
+
+        console.log("Admin created successfully..!");
+        console.log(admin);
+
+        process.exit();
+    }
+    catch(error){
+        console.error("Error creating admin:", error);
+        process.exit(1);
+    }
+}
+
+createAdmin();
